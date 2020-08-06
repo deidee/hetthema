58fc31bf76c7ac9ecceec30bf9ce67fb50311196
acj <gitlab@acjs.net>
2020-08-06 22:06:07 +0200

    Add some basic theme support

--- hetthema/functions.php
+++ hetthema/functions.php
@@ -1,5 +1,23 @@
 <?php
 
+// Enable some basic theme support.
+add_theme_support( 'title-tag' );
+add_theme_support( 'post-thumbnails' );
+add_theme_support( 'html5', array(
+    'comment-list',
+    'comment-form',
+    'search-form',
+    'gallery',
+    'caption',
+    'style',
+    'script' ) );
+
+// Disable smileys (these are not actual emoji).
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
 if(!function_exists('jw')) {
     function jw() {
         ob_start();
