26250977ed37643f13ee94027a2f7ee0acd37c6f
acj <gitlab@acjs.net>
2020-08-07 12:39:17 +0200

    Theme setup

--- functions.php
+++ functions.php
@@ -1,16 +1,26 @@
 <?php
 
-// Enable some basic theme support.
-add_theme_support( 'title-tag' );
-add_theme_support( 'post-thumbnails' );
-add_theme_support( 'html5', array(
-    'comment-list',
-    'comment-form',
-    'search-form',
-    'gallery',
-    'caption',
-    'style',
-    'script' ) );
+function hetthema_setup_theme() {
+	// Enable some basic theme support.
+	// See https://developer.wordpress.org/reference/functions/add_theme_support/.
+	add_theme_support( 'align-wide' );
+	add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'responsive-embeds' );
+	add_theme_support( 'title-tag' );
+	add_theme_support( 'disable-custom-colors' );
+	add_theme_support( 'disable-custom-font-sizes' );
+	add_theme_support( 'html5', array(
+		'comment-list',
+		'comment-form',
+		'search-form',
+		'gallery',
+		'caption',
+		'style',
+		'script' ) );
+
+	load_theme_textdomain( 'hetthema' );
+}
+add_action( 'after_setup_theme', 'hetthema_setup_theme' );
 
 // Disable smileys (these are not actual emoji).
 remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
@@ -20,16 +30,16 @@ remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
 // Apply stylesheet.
 function deidee_scripts() {
-    wp_register_style( 'default-style', 'https://default.style/', '', '1.0.0' );
-    wp_enqueue_style( 'default-style' );
+	wp_register_style( 'default-style', 'https://default.style/', '', '1.0.0' );
+	wp_enqueue_style( 'default-style' );
 }
 add_action( 'wp_enqueue_scripts', 'deidee_scripts' );
 
 if(!function_exists('jw')) {
-    function jw() {
-        ob_start();
-        $expression = func_get_args();
-        call_user_func_array('var_dump', $expression);
-        echo '<pre><code class="language-php">' . htmlentities(ob_get_clean()) . '</code></pre>';
-    }
+	function jw() {
+		ob_start();
+		$expression = func_get_args();
+		call_user_func_array('var_dump', $expression);
+		echo '<pre><code class="language-php">' . htmlentities(ob_get_clean()) . '</code></pre>';
+	}
 }
