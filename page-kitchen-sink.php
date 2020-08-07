fc95e4d38bb7d166b7fe8d6f9d0416a4f13e8ab4
acj <gitlab@acjs.net>
2020-08-07 13:09:45 +0200

    Add kitchen sink

---
+++ page-kitchen-sink.php
@@ -0,0 +1,20 @@
+<?php
+
+get_header();
+
+?>
+	<p>Test. <b>Vergedrukt.</b> <i>Cursief.</i> <em>Benadrukt.</em> <strong>Sterk benadrukt.</strong> <dfn>Definitie.</dfn> <del>Verwijderd.</del> <ins>Toegevoegd.</ins></p>
+	<h2>Lijsten</h2>
+	<ol>
+		<li>Geordende lijst item 1.</li>
+		<li>Geordende lijst item 2.</li>
+		<li>Geordende lijst item 3.</li>
+	</ol>
+	<ul>
+		<li>Ongeordende lijst item 1.</li>
+		<li>Ongeordende lijst item 2.</li>
+		<li>Ongeordende lijst item 3.</li>
+	</ul>
+<?php
+
+get_footer();
