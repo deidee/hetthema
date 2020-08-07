f224278a367709e7271bc37a6fec9e93a03cc91c
acj <gitlab@acjs.net>
2020-08-07 13:12:22 +0200

    Add image to kitchen sink

--- page-kitchen-sink.php
+++ page-kitchen-sink.php
@@ -3,18 +3,23 @@
 get_header();
 
 ?>
-	<p>Test. <b>Vergedrukt.</b> <i>Cursief.</i> <em>Benadrukt.</em> <strong>Sterk benadrukt.</strong> <dfn>Definitie.</dfn> <del>Verwijderd.</del> <ins>Toegevoegd.</ins></p>
-	<h2>Lijsten</h2>
-	<ol>
-		<li>Geordende lijst item 1.</li>
-		<li>Geordende lijst item 2.</li>
-		<li>Geordende lijst item 3.</li>
-	</ol>
-	<ul>
-		<li>Ongeordende lijst item 1.</li>
-		<li>Ongeordende lijst item 2.</li>
-		<li>Ongeordende lijst item 3.</li>
-	</ul>
+    <p>Test. <a href="https://jeidee.nl/">Link.</a> <b>Vergedrukt.</b> <i>Cursief.</i> <em>Benadrukt.</em> <strong>Sterk benadrukt.</strong> <dfn>Definitie.</dfn> <del>Verwijderd.</del> <ins>Toegevoegd.</ins></p>
+    <h2>Lijsten</h2>
+    <ol>
+        <li>Geordende lijst item 1.</li>
+        <li>Geordende lijst item 2.</li>
+        <li>Geordende lijst item 3.</li>
+    </ol>
+    <ul>
+        <li>Ongeordende lijst item 1.</li>
+        <li>Ongeordende lijst item 2.</li>
+        <li>Ongeordende lijst item 3.</li>
+    </ul>
+    <h2>Afbeelding</h2>
+    <figure>
+        <img src="https://picsum.photos/400/300" alt="">
+        <figure>Via <a href="https://picsum.photos/">Lorem Picsum</a>.</figure>
+    </figure>
 <?php
 
 get_footer();
