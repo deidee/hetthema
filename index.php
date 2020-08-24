9391e8130b74841c18263a9630e9f5ce19be85bd
acj <gitlab@acjs.net>
2020-08-24 16:45:51 +0200

    Display heading by default

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
-
 get_header();
-
-if ( have_posts() ) {
-    while ( have_posts() ) {
-        the_post();
-        the_content();
-    }
-}
-
+?>
+<?php if ( have_posts() ): ?>
+	<?php while ( have_posts() ): the_post(); ?>
+		<h1><?php the_title(); ?></h1>
+		<?php the_content(); ?>
+	<?php endwhile; ?>
+<?php endif; ?>
+<?php
 get_footer();
