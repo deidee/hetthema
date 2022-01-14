6fe1c2a9a17a6ed60a1cd623d3d4463cc577ec79
ACJ <gitlab@acjs.net>
2022-01-14 21:45:13 +0100

    Add custom post type: Artwork

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
 
+add_action( 'after_setup_theme', 'hetthema_setup_theme' );
 function hetthema_setup_theme() {
 	// Enable some basic theme support.
 	// See https://developer.wordpress.org/reference/functions/add_theme_support/.
@@ -20,7 +21,6 @@ function hetthema_setup_theme() {
 
 	load_theme_textdomain( 'hetthema' );
 }
-add_action( 'after_setup_theme', 'hetthema_setup_theme' );
 
 // Disable smileys (these are not actual emoji).
 remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
@@ -29,11 +29,11 @@ remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
 // Apply stylesheet.
+add_action( 'wp_enqueue_scripts', 'deidee_scripts' );
 function deidee_scripts() {
 	wp_register_style( 'default-style', 'https://default.style/', '', '1.0.0' );
 	wp_enqueue_style( 'default-style' );
 }
-add_action( 'wp_enqueue_scripts', 'deidee_scripts' );
 
 // Modern Web browsers only allow videos to autoplay when they are inline and muted.
 add_filter( 'wp_video_shortcode', function( $html ) {
@@ -43,6 +43,83 @@ add_filter( 'wp_video_shortcode', function( $html ) {
 
 	return $html;
 } );
+
+add_action( 'init', 'hetthema_ctp_artwork' );
+// See https://developer.wordpress.org/reference/functions/register_post_type/.
+register_post_type('artwork', [
+    'labels' => [
+        'name' => 'Artworks',
+        'singular_name' => 'Artwork',
+        //'add_new' => '',
+        'add_new_item' => 'Add New Artwork',
+        'edit_item' => 'Edit Artwork',
+        'new_item ' => 'New Artwork',
+        'view_item' => 'View Artwork',
+        'view_items' => 'View Artworks',
+        'search_items' => 'Search Artworks',
+        'not_found' => 'No artworks found',
+        'not_found_in_trash' => 'No artworks found in Trash',
+        //'parent_item_colon' => 'Parent Artwork',
+        'all_items' => 'All Artworks',
+        'archives' => 'Artwork Archives',
+        'attributes' => 'Artwork Attributes',
+        'insert_into_item' => 'Insert into artwork',
+        'uploaded_to_this_item' => 'Uploaded to this artwork',
+        //'featured_image' => 'Featured image',
+        //'set_featured_image' => 'Set featured image',
+        //'remove_featured_image' => 'Remove featured image',
+        //'use_featured_image' => 'Use featured image',
+        //'menu_name' => 'Artworks',
+        'filter_items_list' => 'Filter artworks list',
+        //'filter_by_date' => 'Filter by date',
+        'items_list_navigation' => 'Artworks list navigation',
+        'items_list' => 'Artworks list',
+        'item_published' => 'Artwork published',
+        'item_published_privately' => 'Artwork published privately',
+        'item_reverted_to_draft' => 'Artwork reverted to draft',
+        'item_scheduled' => 'Artwork scheduled',
+        'item_updated' => 'Artwork updated',
+        'item_link' => 'Artwork Link',
+        'item_link_description' => 'A link to an artwork'
+    ],
+    //'labels' => [],
+    'description' => 'A work of art.',
+    'public' => true,
+    //'hierarchical' => false,
+    //'exclude_from_search' => false,
+    //'publicly_queryable' => true,
+    //'show_ui' => true,
+    //'show_in_menu' => true,
+    //'show_in_nav_menus' => true,
+    //'show_in_admin_bar' => true,
+    //'show_in_rest' => true,
+    //'rest_base' => 'artist',
+    //'rest_controller_class' => 'WP_REST_Posts_Controller',
+    'menu_position' => 6,
+    // Artist by Gregor Cresnar from NounProject.com
+    'menu_icon' => 'dashicons-art',
+    // 'capability_type',
+    //'capabilities',
+    //'map_meta_cap',
+    'supports' => [
+        'title',
+        'editor',
+        'author',
+        'thumbnail',
+        'excerpt'
+    ],
+    //'register_meta_box_cb'
+    //'taxonomies' => ['series', 'artform'], // TODO
+    //'has_archive' => true,
+    //'rewrite' => [],
+    //'query_var' => 'artist',
+    //'can_export' => true,
+    'delete_with_user' => false,
+    //'template' => [],
+    //'template_lock' => false,
+    //'_builtin' => false,
+    //'_edit_link' => 'post.php?post=%d'
+]);
 
 if(!function_exists('hetthema_lorem_picsum')) {
 	function hetthema_lorem_picsum($width = 512, $height = NULL) {
